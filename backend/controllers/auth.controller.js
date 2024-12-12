ac5af95371a9402fe005d59fc2ad86b14c545694
MatejaP <pavlovicinq@gmail.com>
2024-12-12 20:12:46 +0100

    First commit project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export  const signup = async (req,res) =>{
+    res.json({
+        data: "Signup Route"
+    });
+};
+
+export  const login = async (req,res) =>{
+    res.json({
+        data: "Login Route"
+    });
+};
+
+export  const logout = async (req,res) =>{
+    res.json({
+        data: "Logout Route"
+    });
+};
